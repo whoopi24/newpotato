ca36b97c3f61c66f4e3e145f68784e73722e4df8
whoopi24 <marina.whoopi@gmail.com>
2024-05-04 14:42:58 +0000

    changed output format

--- lsoie_parser_marina.py
+++ lsoie_parser_marina.py
@@ -2,48 +2,46 @@ import pickle
 from collections import defaultdict
 
 import stanza
-from stanza.utils.conll import CoNLL
 from tuw_nlp.text.utils import gen_tsv_sens
 
 # stanza.download("en")
 
 
-def main(input_stream, output_stream, outfile):
+def main(input_stream, outfile):
     nlp = stanza.Pipeline(
         lang="en",
         processors="tokenize,mwt,pos,lemma,depparse",
         tokenize_pretokenized=True,
     )
     stream = open(input_stream, "r", encoding="utf-8")
-    output = open(output_stream, "w", encoding="utf-8")
     mydict = {}
+    iter = 0
     for sen_idx, sen in enumerate(gen_tsv_sens(stream)):
+        iter += 1
         print(f"processing sentence {sen_idx}")
-        # log = open(f"out/test{sen_idx}.log", "w")
         parsed_doc = nlp(" ".join(t[1] for t in sen))
         parsed_sen = parsed_doc.sentences[0]
         args = defaultdict(list)
+        sent = []
         pred = []
         for i, tok in enumerate(sen):
+            sent.append(tok[1])
             label = tok[7].split("-")[0]
             if label == "O":
                 continue
             elif label == "P":
-                pred.append(i + 1)
+                pred.append(i)
                 continue
-            args[label].append(i + 1)
+            args[label].append(i)
 
-        # log.write(f"test{sen_idx} pred: {pred}, args: {args}\n")
-        # CoNLL.write_doc2conll(parsed_doc, f"out/test{sen_idx}.conll")
-        # log.write(f"wrote parse to test{sen_idx}.conll\n")
+        mydict[sen_idx] = {"sent": sent, "pred": pred, "args": args}
 
-        output.write(f"test{sen_idx} sent: {parsed_sen.text}\n")
-        output.write(f"test{sen_idx} pred: {pred}, args: {args}\n")
-
-        mydict[f"test{sen_idx}"] = parsed_sen
+        if iter == 20:
+            break
 
     with open(outfile, "wb") as f:
         pickle.dump(mydict, f)
+        print("dictionary saved successfully to file")
 
     # with open(outfile, "rb") as f:
     #     loaded_dict = pickle.load(f)
@@ -51,6 +49,5 @@ def main(input_stream, output_stream, outfile):
 
 if __name__ == "__main__":
     input_stream = "lsoie_wiki_dev.conll"
-    output_stream = "output.log"
-    outfile = "output.pkl"
-    main(input_stream, output_stream, outfile)
+    outfile = "sample.pkl"
+    main(input_stream, outfile)
