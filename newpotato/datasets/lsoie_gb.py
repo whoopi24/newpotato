68f6dc06abf251bf34779ede20f9770667dc730f
whoopi24 <marina.whoopi@gmail.com>
2025-01-13 02:46:59 +0000

    removed replace argument and hgraph creation in lsoie_gb.py

--- newpotato/datasets/lsoie_gb.py
+++ newpotato/datasets/lsoie_gb.py
@@ -1,12 +1,8 @@
 import argparse
 import logging
-import os
-import re
 from collections import defaultdict
 
 import networkx as nx
-from graphbrain import hgraph
-from graphbrain.utils.conjunctions import conjunctions_decomposition
 from rich.console import Console
 from tqdm import tqdm
 from tuw_nlp.text.utils import gen_tsv_sens
@@ -17,70 +13,9 @@ from newpotato.hitl_marina import HITLManager
 console = Console()
 
 
-def replace_atom_with_annotation(edge, to_replace, replacement, unique=False):
-    atoms = edge.all_atoms()  # 'graphbrain.hyperedge.Atom'
-    sum = 0
-    for atom in atoms:
-        if atom.root() == to_replace.lower():
-            sum += 1
-            found = atom  # saves first match
-            break
-    if sum == 0:
-        return edge
-    elif sum == 1 or unique == False:
-        newatom = found.replace_atom_part(0, replacement)
-        newedge = edge.replace_atom(
-            found, newatom, unique=False
-        )  # replaces all occurences with newatom when argroles match
-        return newedge
-    else:
-        # TODO: find correct atom when there are multiple atoms found
-        print("multiple atoms found")
-
-
-def extract_subedges(hyperedge, max_depth=1):
-    hyperedge = str(hyperedge)
-    subedges = []
-    stack = []
-    current = ""
-    current_depth = 0  # Track the current depth
-
-    for char in hyperedge:
-        if char == "(":
-            if current_depth < max_depth:
-                if stack:  # If already inside a parenthesis, save to current
-                    current += char
-                stack.append("(")
-                current_depth += 1
-            else:
-                current += char  # Beyond max_depth, add to the current subedge
-        elif char == ")":
-            if current_depth <= max_depth:
-                if stack:
-                    stack.pop()
-                if stack:
-                    current += char
-                else:
-                    # End of a subedge within max_depth
-                    subedges.append(current.strip())
-                    current = ""
-            else:
-                current += char
-            current_depth = len(stack)  # Update depth based on stack
-        else:
-            if stack:
-                current += char
-
-    return subedges
-
-
-def load_and_map_lsoie(input_file, extractor, replace_yn=False, path="hg_test.db"):
+def load_and_map_lsoie(input_file, extractor):
     with open(input_file) as stream:
         total, skipped = 0, 0
-        # creates hypergraph for replaced (sub)edges
-        if os.path.exists(path):
-            os.remove(path)
-        hg = hgraph(path)
         for sen_idx, sen in tqdm(enumerate(gen_tsv_sens(stream))):
             total += 1
             words = [t[1] for t in sen]
@@ -126,6 +61,8 @@ def load_and_map_lsoie(input_file, extractor, replace_yn=False, path="hg_test.db
             triplet = Triplet(pred, args, toks=words)
             try:
                 mapped_triplet = extractor.map_triplet(triplet, sentence, strict=False)
+                # mapped_triplet is of class newpotato.extractors.graphbrain_extractor_PC.GraphbrainMappedTriplet
+                # mapped_triplet has ".variables" attribute
             except (KeyError, nx.exception.NetworkXPointlessConcept):
                 logging.error(f"error mapping triplet: {triplet=}, {words=}")
                 logging.error("skipping")
@@ -133,7 +70,8 @@ def load_and_map_lsoie(input_file, extractor, replace_yn=False, path="hg_test.db
                 logging.error(f"{skipped=}, {total=}")
                 continue
 
-            # print(mapped_triplet.variables)
+            yield sentence, mapped_triplet
+
             # mapped_triplet.variables:
             # {'REL': dissolved/Pd.xso.<f-----/en,
             # 'ARG0': ('s/Bp.am/en (the/Md/en country/Cc.s/en) parliament/Cc.s/en),
@@ -141,80 +79,9 @@ def load_and_map_lsoie(input_file, extractor, replace_yn=False, path="hg_test.db
             # 'ARG2': (earlier/M=/en today/Cc.s/en),
             # 'ARG3': formally/M/en}
 
-            if replace_yn:
-                newedge = graph
-                # print(newedge)
-                for p in pred:
-                    to_replace = words[p]
-                    # print(to_replace)
-                    replacement = "REL"
-                    newedge = replace_atom_with_annotation(
-                        newedge, to_replace, replacement, unique=False
-                    )
-                count = 0
-                for arg in args:
-                    replacement = "A" + str(count)
-                    # print(replacement)
-                    for a in arg:
-                        to_replace = words[a]
-                        # print(to_replace)
-                        newedge = replace_atom_with_annotation(
-                            newedge, to_replace, replacement, unique=False
-                        )
-                    count += 1
-                # print(newedge)
 
-                # exclusion of conjunctions
-                edges = conjunctions_decomposition(newedge, concepts=True)
-                # only add new edge to hg if all annotations are included
-                arg_cnt = len(args)
-                for newedge in edges:
-                    # find roots with ARG
-                    roots = {atom.root() for atom in newedge.atoms()}
-                    pattern = r"^A[0-5]"
-                    matches = [a for a in roots if re.match(pattern, a)]
-                    cnt = len(matches)
-                    if cnt == arg_cnt and "REL" in roots:
-                        # TODO: type inference rules (p. 8) for equal annotations
-                        newedge = newedge.simplify(
-                            subtypes=False, argroles=False, namespaces=False
-                        )
-                        subedges = extract_subedges(newedge)
-                        # print(subedges)
-
-                        # save new edge with annotations
-                        hg.add(newedge)
-
-                        # save patterns (use count or sth similar)
-                    else:
-                        # print("skipped")
-                        continue
-
-                # TODO: how to re-build entire hyperedge again after replacing several subedges?
-                # TODO: unite atoms with equal annotations
-                # subedges = newedge.subedges()
-                # for se in subedges:
-                #     if se.atom:
-                #         continue
-                #     print("se:", se)
-                #     atoms = se.all_atoms()
-                #     roots = {atom.root() for atom in atoms}
-                #     if len(set(roots)) == 1:
-                #         print("can be united")
-                #         print(se.type()) # or mtype() ??
-                #         newatom = roots[0] + "/" + se.mtype()
-                #         newedge = se.replace_atom(se, newatom, unique=False)
-                #         print(newatom)
-                #         print(newedge)
-
-            else:
-                hg.add(graph)
-
-            yield sentence, mapped_triplet
-
-
-def load_lsoie_to_hitl(input_file, hitl, replace_yn):
-    for sen, triplet in load_and_map_lsoie(input_file, hitl.extractor, replace_yn):
+def load_lsoie_to_hitl(input_file, hitl):
+    for sen, triplet in load_and_map_lsoie(input_file, hitl.extractor):
         hitl.store_triplet(sen, triplet, True)
 
 
@@ -224,7 +91,6 @@ def get_args():
     parser.add_argument("-v", "--verbose", action="store_true")
     parser.add_argument("-i", "--input_file", default=None, type=str)
     parser.add_argument("-s", "--state_file", default=None, type=str)
-    parser.add_argument("-r", "--replace", default=False, type=bool)
     return parser.parse_args()
 
 
@@ -243,7 +109,7 @@ def main():
     console.print("initializing HITL session")
     hitl = HITLManager(extractor_type="graphbrain")
     console.print(f"loading LSOIE data from {args.input_file}")
-    load_lsoie_to_hitl(args.input_file, hitl, args.replace)
+    load_lsoie_to_hitl(args.input_file, hitl)
     console.print(f"saving HITL session to {args.state_file}")
     hitl.save(args.state_file)
 
