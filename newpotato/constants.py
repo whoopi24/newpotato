5ef9b0938d55c2ba9b75ff2a2caef136876da838
whoopi24 <marina.whoopi@gmail.com>
2025-01-19 07:11:10 +0000

    added elements to NON_WORD_ATOMS

--- newpotato/constants.py
+++ newpotato/constants.py
@@ -1,3 +1,16 @@
-NON_WORD_ATOMS = {"list/J/.", "+/B.am/.", "+/B.mm/.", "+/B.ma/.", ":/J/.", "+", ":"}
+# added +/B.aa/., :/Jr.ma, +/B/., +/J.mm/.
+NON_WORD_ATOMS = {
+    "list/J/.",
+    "+/B/.",
+    "+/B.aa/.",
+    "+/B.am/.",
+    "+/B.mm/.",
+    "+/B.ma/.",
+    ":/J/.",
+    ":/Jr.ma",
+    "+/J.mm/.",
+    "+",
+    ":",
+}
 
 NON_ATOM_WORDS = {'"', "'", ">", "<", "(", ")", "-", ","}
